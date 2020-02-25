5980bead8eb56fa9001cc97bf349f04ece164966
zheng <zheng@LAPTOP-D919HKR1>
2020-02-24 21:36:17 -0500

    this is a test

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This is a test");
+
             Console.WriteLine("Hello World!");
         }
     }
